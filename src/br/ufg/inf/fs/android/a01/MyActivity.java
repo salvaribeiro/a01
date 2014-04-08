f0cab526749380b31c46653a75082bf042fbe12c
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2014-04-08 17:39:31 -0300

    Opções do menu criadas e exibidas quando aplicativo é iniciado.

--- src/br/ufg/inf/fs/android/a01/MyActivity.java
+++ src/br/ufg/inf/fs/android/a01/MyActivity.java
@@ -2,6 +2,8 @@ package br.ufg.inf.fs.android.a01;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +13,19 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    /**
+     * Chamado para "inflar" o recurso "menu" contendo
+     * os itens do menu de opções.
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
     }
 }
